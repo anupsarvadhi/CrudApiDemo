9e11f5efd1947b4c3ca65577d4ed3875e705498b
anupsarvadhi <anup.chopde@sarvadhi.com>
2022-08-16 10:32:58 +0530

    second commit

--- src/componant/CommonHeader.js
+++
@@ -1,8 +0,0 @@
- const Header = () =>{
-  headers: {
-    'Content-Type': 'application/json',
-    Authorization:
-      'Bearer 48af1cd25351a38499abef9068a6168aa0ac9cc85523a78f11b0da2ef26a7764',
-    'Access-Control-Allow-Origin': 'http://localhost:3000',
-  }
- }
